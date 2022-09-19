bf3126f4673fbfe83c71fd139699a249cf2e85b6
fruitfly638 <moritz.kodytek@suse.com>
2022-09-19 10:33:30 +0200

    Add more precise AI player check
    Alos some readability changes

--- main.py
+++ main.py
@@ -30,8 +30,8 @@ def get_new_games(username, games):
 
 def is_loss(png, username):
     """Check if a game is a loss."""
-    if "ai" not in str(png):
-        if png["status"] != "draw" and png["status"] != "timeout" and png["status"] != "stalemate" and png["status"] != "outoftime":
+    if "user" in png["players"]['white'] and "user" in png["players"]['black']:
+        if png["status"] not in ["draw", "timeout", "stalemate", "outoftime"]:
             if png['winner'] == 'white' and png["players"]['white']["user"]["name"] == username:
                 return True
             elif png['winner'] == 'black' and png["players"]['black']["user"]["name"] == username:
